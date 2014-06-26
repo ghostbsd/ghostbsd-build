469a1df4a89520b7b041cb459a06cd89735f51ea
ericbsd <ericturgeon.bsd@gmail.com>
2014-06-26 10:37:24 -0300

    Change code not build architectur file in /usr/obj

--- release-out.py
+++ release-out.py
@@ -1,29 +1,30 @@
 #!/usr/local/bin/python
 
 import pexpect
-from subprocess import call
-import getpass
+from subprocess import Popen, PIPE
+from getpass import getpass
 
 i386 = "/usr/obj/i386"
 amd64 = "/usr/obj/amd64"
 server = "frs.sourceforge.net"
 user = raw_input("username: ")
-password = getpass.getpass("Password: ")
+password = getpass("Password: ")
 
-call('cd /usr/obj && ls GhostBSD*i386* > %s' % i386, shell=True)
-call('cd /usr/obj && ls GhostBSD*amd64* > %s' % amd64, shell=True)
+i386 = Popen('cd /usr/obj && ls GhostBSD*i386*', stdout=PIPE, shell=True)
+amd64 = Popen('cd /usr/obj && ls GhostBSD*amd64*', stdout=PIPE, shell=True)
 
-release = open(i386, "r")
-for line in release.readlines():
-    foo = pexpect.spawn('scp /usr/obj/%s %s@%s:/home/frs/project/g/gh/ghostbsdproject/release/i386/4.0/%s'
+
+for line in i386.stdout.readlines():
+    foo = pexpect.spawn(
+    'scp /usr/obj/%s %s@%s:/home/frs/project/g/gh/ghostbsdproject/release/i386/4.0/%s'
     % (line.strip(), user, server, line.strip()))
     foo.expect('Password:')
     foo.sendline(password)
     foo.interact()
 
-release = open(amd64, "r")
-for line in release.readlines():
-    foo = pexpect.spawn('scp /usr/obj/%s %s@%s:/home/frs/project/g/gh/ghostbsdproject/release/amd64/4.0/%s'
+for line in amd64.stdout.readlines():
+    foo = pexpect.spawn(
+    'scp /usr/obj/%s %s@%s:/home/frs/project/g/gh/ghostbsdproject/release/amd64/4.0/%s'
     % (line.strip(), user, server, line.strip()))
     foo.expect('Password:')
     foo.sendline(password)
