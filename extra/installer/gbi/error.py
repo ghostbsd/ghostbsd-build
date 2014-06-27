ca699b20dcd636a3dc97696d3192cd76b4ab1e03
ericbsd <ericturgeon.bsd@gmail.com>
2014-06-27 10:32:24 -0300

    Fix new text to error.py and added the link to the new issue report system.

--- extra/installer/gbi/error.py
+++ extra/installer/gbi/error.py
@@ -7,8 +7,8 @@ import gtk
 from subprocess import Popen
 
 lyrics = """
-Installation has failed! Please report to
-http://ghostbsd.org/problem_report, and be sure
+ Please report to
+, and be sure
 to provide /tmp/.pc-sysinstall/pc-sysinstall.log.
 """
 
@@ -27,7 +27,7 @@ class PyApp:
         window.set_border_width(8)
         window.connect("destroy", gtk.main_quit)
         window.set_title("Installation Error")
-        window.set_icon_from_file("/usr/local/etc/gbi/logo.png")
+        #window.set_icon_from_file("/usr/local/etc/gbi/logo.png")
         box1 = gtk.VBox(False, 0)
         window.add(box1)
         box1.show()
@@ -35,7 +35,14 @@ class PyApp:
         box2.set_border_width(10)
         box1.pack_start(box2, True, True, 0)
         box2.show()
+        title = gtk.Label()
+        title.set_use_markup(True)
+        title.set_markup('<b><span size="larger">Installation has failed!</span></b>')
         label = gtk.Label(lyrics)
+        label.set_use_markup(True)
+
+        label.set_markup("Please report the issue to <a href='http://issues.ghostbsd.org/my_view_page.php'>GhostBSD issue system</a>,\nand be sure to provide tmp/.pc-sysinstall/pc-sysinstall.log.")
+        box2.pack_start(title)
         box2.pack_start(label)
         box2 = gtk.HBox(False, 10)
         box2.set_border_width(5)
