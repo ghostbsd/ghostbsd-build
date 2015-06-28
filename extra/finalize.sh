fe1c81a89386dff609c0eb0ce5e96fc0459a4d27
angelescuo <angelescuo@yahoo.com>
2015-06-28 14:14:30 +0300

    readded code to clean ShowIn=Gnome  ShowIn=Mate in *desktop files ( needed by update-station)

--- extra/finalize.sh
+++ extra/finalize.sh
@@ -15,24 +15,59 @@ if [ -z "${LOGFILE:-}" ]; then
     exit 1
 fi
 
-# Remove gmplayer.desktop
-if [ -f "/usr/local/share/applications/gmplayer.desktop" ]; then
-  rm ${BASEDIR}/usr/local/share/applications/gmplayer.desktop
-fi
+remove_desktop_entries()
+{
+# Remove unneeded *.desktop
+for rfile in gmplayer spideroak ; do
+    if [ -f "/usr/local/share/applications/${rfile}.desktop" ]; then
+        rm ${BASEDIR}/usr/local/share/applications/gmplayer.desktop
+    fi
+done
+}
 
+cursor_theme()
+{
 # Set cursor theme instead of default from xorg
+# to do with alternatives if possible from common installed settings
 if [ -e ${BASEDIR}/usr/local/lib/X11/icons/default ] ; then
 rm ${BASEDIR}/usr/local/lib/X11/icons/default 
 fi
 cd ${BASEDIR}/usr/local/lib/X11/icons
 ln -sf $CURSOR_THEME default
 cd -
+}
+
+rm_fbsd_pcsysinstall()
+{
 # Setting installer
 rm -rf ${BASEDIR}/usr/sbin/pc-sysinstall
 rm -rf ${BASEDIR}/usr/share/pc-sysinstall
+}
 
-# enable pcdm if installed
+dm_enable()
+{
+# enable display manager if installed
 if [ -e $(which pcdm) ] ; then 
     sed -i '' 's@#pcdm_enable="YES"@pcdm_enable="YES"@g' ${BASEDIR}/etc/rc.conf
 fi
+}
 
+clean_desktop_files()
+{
+# Remove Gnome and Mate from ShowOnly in *.desktop
+# needed for update-station
+GhostBSD=`ls /usr/local/share/applications/ | grep -v libreoffice | grep -v kde4 | grep -v screensavers` 
+for desktop in $GhostBSD; do
+  sed -i "" -e 's/OnlyShowIn=Gnome;//g' /usr/local/share/applications/$desktop
+  sed -i "" -e 's/OnlyShowIn=MATE;//g' /usr/local/share/applications/$desktop
+  sed -i "" -e 's/GNOME;//g' /usr/local/share/applications/$desktop
+  sed -i "" -e 's/MATE;//g' /usr/local/share/applications/$desktop
+  sed -i "" -e 's/OnlyShowIn=//g' /usr/local/share/applications/$desktop
+done
+}
+
+remove_desktop_entries
+clean_desktop_files
+rm_fbsd_sysinstall
+cursor_theme
+dm_enable
