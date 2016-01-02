43faac79661caff3d484cd4d22cfd67754d72c55
Angelescu Ovidiu <angelescuo@yahoo.com>
2016-01-02 17:53:12 +0200

    added preliminary uefi support for make standard iso and make standard img

--- scripts/iso.sh
+++ scripts/iso.sh
@@ -15,18 +15,20 @@ if [ -z "${LOGFILE:-}" ]; then
     exit 1
 fi
 
+GHOSTBSD_LABEL=`echo $GHOSTBSD_LABEL | tr '[:lower:]' '[:upper:]'`
+
 echo "#### Building bootable ISO image for ${ARCH} ####"
 # Creates etc/fstab to avoid messages about missing it
 if [ ! -e ${BASEDIR}/etc/fstab ] ; then
     touch ${BASEDIR}/etc/fstab
 fi
 
+echo "/dev/iso9660/$GHOSTBSD_LABEL / cd9660 ro 0 0" > $BASEDIR/etc/fstab
+
 cd ${BASEDIR} && tar -cpzf ${BASEDIR}/dist/etc.tgz etc
 
 make_standard_iso()
 {
-GHOSTBSD_LABEL=GHOSTBSD
-echo "/dev/iso9660/$GHOSTBSD_LABEL / cd9660 ro 0 0" > ${BASEDIR}/etc/fstab 
 echo "### Running makefs to create ISO ###"
 bootable="-o bootimage=i386;${BASEDIR}/boot/cdboot -o no-emul-boot"
 makefs -t cd9660 $bootable -o rockridge -o label=${GHOSTBSD_LABEL} ${ISOPATH} ${BASEDIR}
@@ -44,6 +46,28 @@ if [ $? -ne 0 ] ; then
 fi
 }
 
+make_standard_uefi_iso()
+{
+# Make EFI system partition (should be done with makefs in the future)
+dd if=/dev/zero of=efiboot.img bs=4k count=100
+device=`mdconfig -a -t vnode -f efiboot.img`
+newfs_msdos -F 12 -m 0xf8 /dev/$device
+mkdir efi
+mount -t msdosfs /dev/$device efi
+mkdir -p efi/efi/boot
+cp ${BASEDIR}/boot/loader.efi efi/efi/boot/bootx64.efi
+umount efi
+rmdir efi
+mdconfig -d -u $device
+
+echo $UEFI_ISOPATH
+echo " making uefi iso"
+bootable="-o bootimage=i386;efiboot.img -o no-emul-boot"
+makefs -t cd9660 $bootable -o rockridge -o label=${GHOSTBSD_LABEL} ${UEFI_ISOPATH} ${BASEDIR}
+rm -f efiboot.img
+echo "uefi iso done"
+}
+
 echo "### ISO created ###"
 
 # Make md5 and sha256 for iso
@@ -52,12 +76,22 @@ make_checksums()
 cd /usr/obj/${ARCH}/${PACK_PROFILE}
 md5 `echo ${ISOPATH}|cut -d / -f6`  >> /usr/obj/${ARCH}/${PACK_PROFILE}/$(echo ${ISOPATH}|cut -d / -f6).md5
 sha256 `echo ${ISOPATH}| cut -d / -f6` >> /usr/obj/${ARCH}/${PACK_PROFILE}/$(echo ${ISOPATH}|cut -d / -f6).sha256
+
+if [ "$ARCH" = "amd64" ]; then
+    md5 `echo ${UEFI_ISOPATH}|cut -d / -f6`  >> /usr/obj/${ARCH}/${PACK_PROFILE}/$(echo ${UEFI_ISOPATH}|cut -d / -f6).md5
+    sha256 `echo ${UEFI_ISOPATH}| cut -d / -f6` >> /usr/obj/${ARCH}/${PACK_PROFILE}/$(echo ${UEFI_ISOPATH}|cut -d / -f6).sha256
+fi
 cd -
 }
 
+
 #make_standard_iso
 make_grub_iso
+if [ "${ARCH}" = "amd64" ]; then
+    make_standard_uefi_iso
+fi
 make_checksums
+
 
 set -e
 cd ${LOCALDIR}
