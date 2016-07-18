8d2ff6d00362031274eb1ec6a93d92e9f34c90de
Ovidiu Angelescu <angelescuo@yahoo.com>
2016-07-18 10:51:17 +0300

    changed rc.conf for virtuoso config file

--- packages/packages.cfg/virtuoso.sh
+++ packages/packages.cfg/virtuoso.sh
@@ -5,5 +5,5 @@ if [ -d /usr/local/lib/virtuoso ] ; then
     cp /usr/local/lib/virtuoso/db/virtuoso.ini.sample /usr/local/lib/virtuoso/db/virtuoso.ini
     # enable virtuoso in rc.conf
     echo 'virtuoso_enable="YES"' >> /etc/rc.conf
-    #echo 'virtuoso_config="/usr/local/lib/virtuoso/db/virtuoso.ini"' >> /etc/rc.conf
+    echo 'virtuoso_config="/usr/local/lib/virtuoso/db/virtuoso.ini"' >> /etc/rc.conf
 fi
