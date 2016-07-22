de385f9d65296d8979b264f4090c66fca1e71edd
Ovidiu Angelescu <angelescuo@yahoo.com>
2016-07-22 20:13:10 +0300

    disabled virtuoso in live mode

--- packages/packages.cfg/virtuoso.sh
+++ packages/packages.cfg/virtuoso.sh
@@ -4,6 +4,6 @@ if [ -d /usr/local/lib/virtuoso ] ; then
     # copy virtuoso config file
     cp /usr/local/lib/virtuoso/db/virtuoso.ini.sample /usr/local/lib/virtuoso/db/virtuoso.ini
     # enable virtuoso in rc.conf
-    echo 'virtuoso_enable="YES"' >> /etc/rc.conf
+    echo 'virtuoso_enable="NO"' >> /etc/rc.conf
     echo 'virtuoso_config="/usr/local/lib/virtuoso/db/virtuoso.ini"' >> /etc/rc.conf
 fi
