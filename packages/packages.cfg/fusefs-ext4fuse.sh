a7870143fa3a1e986c34e8ab00e9c3e450395fa4
ericbsd <ericturgeon.bsd@gmail.com>
2017-01-15 19:56:03 -0400

    removing make_system

--- packages/packages.cfg/fusefs-ext4fuse.sh
+++ packages/packages.cfg/fusefs-ext4fuse.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-# load fuse kernel module for all fuse* progs
-grep -q "fuse_load" /boot/loader.conf
-if [ $? -ne 0 ]; then
-    echo 'fuse_load="YES"' >> /boot/loader.conf
-fi
-
-grep -q "fuse" /boot/grub/grub.cfg
-if [ $? -ne 0 ]; then
-    sed -i '' '/set kFreeBSD.kern.vty=vt/a\
-\  kfreebsd_module_elf /boot/kernel/fuse.ko\
-' /boot/grub/grub.cfg
-fi
-
 if [ -e /usr/local/bin/ext4fuse ]; then
     ln -s /usr/local/bin/ext4fuse /sbin/mount_ext4fs
 fi
--- packages/packages.cfg/fusefs-ntfs.sh
+++ packages/packages.cfg/fusefs-ntfs.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-# load fuse kernel module for all fuse* progs
-grep -q "fuse_load" /boot/loader.conf
-if [ $? -ne 0 ]; then
-    echo 'fuse_load="YES"' >> /boot/loader.conf
-fi
-
-grep -q "fuse" /boot/grub/grub.cfg
-if [ $? -ne 0 ]; then
-    sed -i '' '/set kFreeBSD.kern.vty=vt/a\
-\  kfreebsd_module_elf /boot/kernel/fuse.ko\
-' /boot/grub/grub.cfg
-fi 
-
 if [ -e /usr/local/bin/ntfs-3g ]; then
     ln -s /usr/local/bin/ntfs-3g /sbin/mount_ntfs
 fi
