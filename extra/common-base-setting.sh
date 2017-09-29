f682ffd07133283732dff673038493c566376e00
ericbsd <ericturgeon.bsd@gmail.com>
2017-09-29 11:58:26 -0300

    add missing local_files function

--- extra/common-base-setting.sh
+++ extra/common-base-setting.sh
@@ -21,7 +21,8 @@ patch_etc_files()
 local_files()
 {
   cp extra/common-base-setting/etc/grub.d/10_kghostbsd ${BASEDIR}/usr/local/etc/grub.d/10_kghostbsd
-  sed -i "" -e 's/"\/usr\/local\/sbin\/beadm"/"\/usr\/local\/etc\/grub.d\/10_kghostbsd"/g' ${BASEDIR}/usr/local/etc/grub.d/10_kfreebsd
+  sed -i "" -e
+  's/"\/usr\/local\/sbin\/beadm"/"\/usr\/local\/etc\/grub.d\/10_kghostbsd"/g' ${BASEDIR}/usr/local/etc/grub.d/10_kfreebsd
 }
 
 packages_settings()
@@ -38,4 +39,5 @@ base_overrides
 patch_etc_files
 # apply packages settings
 #packages_settings
+local_files
 
