f77476206fc24b30352490fbca4af3231ad00765
ericbsd <ericturgeon.bsd@gmail.com>
2019-02-03 20:58:59 -0400

    update lightdm code

--- systems/trueos/extra/dm.sh
+++ systems/trueos/extra/dm.sh
@@ -4,21 +4,13 @@ set -e -u
 
 lightdm_setup()
 {
-  if [ -f ${release}/usr/local/etc/lightdm/lightdm.conf ] ; then
-    sed -i "" '/#exit-on-failure=false/a\
-autologin-user=${liveuser}\
-autologin-user-timeout=0\
-' ${release}/usr/local/etc/lightdm/lightdm.conf
-  fi
-
   if [ -f ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf ] ; then
-    echo "background=/usr/local/share/backgrounds/ghostbsd/White-Trees-Empire.jpg" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
+    echo "background=/usr/local/share/backgrounds/ghostbsd/Tidepool_Sunset.png" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
     echo "user-background=true" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
     #echo "theme-name=Ambiance-Blackout-Flat-Aqua" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
     #echo "icon-theme-name=Vivacious-Colors-Full-Dark" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
   fi
   setup_xinit
-
 }
 
 gdm_setup()
