f1a2119303fe6b86fe15439ff6dcecba730787ef
ericbsd <ericturgeon.bsd@gmail.com>
2019-02-03 21:51:38 -0400

    clean common-base-setting code

--- systems/trueos/extra/common-base-setting.sh
+++ systems/trueos/extra/common-base-setting.sh
@@ -7,24 +7,10 @@ patch_etc_files()
   cat ${cwd}/systems/trueos/extra/common-base-setting/patches/boot/loader.conf.extra >> ${release}/boot/loader.conf
   cat ${cwd}/systems/trueos/extra/common-base-setting/patches/etc/profile.extra >> ${release}/etc/profile
   cat ${cwd}/systems/trueos/extra/common-base-setting/patches/etc/devfs.rules.extra >> ${release}/etc/devfs.rules
-  cat ${cwd}/systems/trueos/extra/common-base-setting/patches/etc/sysctl.conf.extra >> ${release}/etc/sysctl.conf
   cat ${cwd}/systems/trueos/extra/common-base-setting/patches/etc/fstab.extra >> ${release}/etc/fstab
-  cd ${release}/etc/
-  patch < ${cwd}/systems/trueos/extra/common-base-setting/patches/etc/login.conf.diff
-  cd -
-  chroot ${release} cap_mkdb /etc/login.conf
-}
-
-packages_settings()
-{
-  #set htmlview alternative to firefox for cups
-  if [ -e ${release}/usr/local/bin/firefox ] ; then
-    update-alternatives --altdir ${release}/usr/local/etc/alternatives --install ${release}/usr/local/bin/htmlview htmlview ${release}/usr/local/bin/firefox 50
-  fi
 }
 
 setup_base()
 {
   patch_etc_files
-  packages_settings
 }
