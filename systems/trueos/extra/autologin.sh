a8e162d51c406ba0866b8e53fe71e982da381b99
ericbsd <ericturgeon.bsd@gmail.com>
2019-02-05 08:26:33 -0400

    clean loader.conf

--- systems/trueos/extra/autologin.sh
+++ systems/trueos/extra/autologin.sh
@@ -8,15 +8,7 @@ setup_autologin()
   echo "# ${liveuser} user autologin" >> ${release}/etc/gettytab
   echo "${liveuser}:\\" >> ${release}/etc/gettytab
   echo ":al=${liveuser}:ht:np:sp#115200:" >> ${release}/etc/gettytab
-
   sed -i "" "/ttyv0/s/Pc/${liveuser}/g" ${release}/etc/ttys
   # echo "sh sysconfig.sh" >> ${release}/root/.login
   echo "startx" >> ${release}/usr/home/${liveuser}/.login
-  # echo 'if ($tty == ttyv0) then' >> ${release}/usr//home/${liveuser}/.cshrc
-  # echo 'if ($tty == ttyv0) then' >> ${release}/usr//home/${liveuser}/.shrc
-  # echo "  sudo netcardmgr" >> ${release}/usr/home/${liveuser}/.cshrc
-  # echo "  startx" >> ${release}/usr/home/${liveuser}/.cshrc
-  # echo "  startx" >> ${release}/usr/home/${liveuser}/.shrc
-  # echo "endif" >> ${release}/usr/home/${liveuser}/.cshrc
-  # echo "endif" >> ${release}/usr/home/${liveuser}/.shrc
 }
