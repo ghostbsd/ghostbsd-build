bb0f27309db8e98f0c1788e5db8e445f257224ad
ericbsd <ericturgeon.bsd@gmail.com>
2019-02-05 17:40:52 -0400

    adding config.fish to execute .login

--- systems/trueos/extra/autologin.sh
+++ systems/trueos/extra/autologin.sh
@@ -10,5 +10,7 @@ setup_autologin()
   echo ":al=${liveuser}:ht:np:sp#115200:" >> ${release}/etc/gettytab
   sed -i "" "/ttyv0/s/Pc/${liveuser}/g" ${release}/etc/ttys
   # echo "sh sysconfig.sh" >> ${release}/root/.login
+  mkdir -p ${release}/usr/home${liveuser}/.config/fish
+  echo 'exec sh ~/.login' >  ${release}/usr/home${liveuser}/.config/fish/config.fish
   echo "startx" >> ${release}/usr/home/${liveuser}/.login
 }
