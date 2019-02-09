58259095243aed02e77250c2390227d96d29b8ff
ericbsd <ericturgeon.bsd@gmail.com>
2019-02-09 12:00:56 -0400

    remove autologin group from build and remove code to default drive

--- systems/trueos/extra/setuser.sh
+++ systems/trueos/extra/setuser.sh
@@ -4,10 +4,6 @@ set -e -u
 
 setup_liveuser()
 {
-  chroot ${release} pw mod user ${liveuser} -w none
-  chroot ${release} pw groupadd autologin
-  chroot ${release} pw groupmod autologin -M ${liveuser}
-
   chroot ${release} su ${liveuser} -c "mkdir -p /usr/home/${liveuser}/Desktop"
   chroot ${release} su ${liveuser} -c "/usr/local/bin/xdg-user-dirs-update"
 
