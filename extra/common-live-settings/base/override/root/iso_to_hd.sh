e8abb616035d14d6b8aa4719f9f5bfeedfd4e119
ericbsd <ericturgeon.bsd@gmail.com>
2019-04-09 17:16:09 -0300

    fix iso_to_hd.sh

--- extra/common-live-settings/base/override/root/iso_to_hd.sh
+++ extra/common-live-settings/base/override/root/iso_to_hd.sh
@@ -76,7 +76,6 @@ setup_lightdm_and_xinitrc()
         echo 'exec cinnamon-session' > /usr/home/${user}/.xinitrc
         chown ${user}:wheel /usr/home/${user}/.xinitrc
       done ;;
-      ;;
   esac
   rc-update add lightdm default
 }
