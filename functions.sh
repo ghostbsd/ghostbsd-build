03494df8e99e4b38d763fbb82292bd072d521d04
ericbsd <ericturgeon@ghostbsd.org>
2019-08-02 17:52:17 -0300

    change the code to copy the whole kernel on the livecd

--- functions.sh
+++ functions.sh
@@ -244,10 +244,7 @@ mfs()
 boot()
 {
   cd "${release}"
-  tar -cf - --exclude boot/kernel boot | tar -xf - -C "${cdroot}"
-  for kfile in kernel geom_uzip.ko nullfs.ko tmpfs.ko geom_mirror.ko crypto.ko geom_eli.ko aesni.ko; do
-    tar -cf - boot/kernel/${kfile} | tar -xf - -C "${cdroot}"
-  done
+  tar -cf - boot | tar -xf - -C "${cdroot}"
   cd "${cwd}"
   cp -R boot/ ${cdroot}/boot/
   mkdir ${cdroot}/etc
