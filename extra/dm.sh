01912753772aa6426fb23a6da6e11a3e9874be88
ericbsd <ericturgeon.bsd@gmail.com>
2019-08-28 08:38:10 -0300

    adding missing lightdm setting

--- extra/dm.sh
+++ extra/dm.sh
@@ -5,17 +5,20 @@ set -e -u
 lightdm_setup()
 {
    if [ "${desktop}" == "xfce" ] ; then
+    sed -i '' "s@#user-session=default@user-session=xfce@" ${release}/usr/local/etc/lightdm/lightdm.conf
     if [ -f ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf ] ; then
       echo "indicators=~host;~spacer;~clock;~spacer;~session;~language;~a11y;~sound;~power" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
       echo "background=/usr/local/share/backgrounds/ghostbsd/Tidepool_Sunset.jpg" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
       echo "theme-name=Vimix-Dark" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
       echo -e "icon-theme-name=Vivacious-Colors-Full-Dark\n" >> ${release}/usr/local/etc/lightdm/lightdm-gtk-greeter.conf
     fi
-  else
+  elif [ "${desktop}" == "mate" ] ; then
     sed -i '' "s@#greeter-session=example-gtk-gnome@greeter-session=slick-greeter@" ${release}/usr/local/etc/lightdm/lightdm.conf
+    sed -i '' "s@#user-session=default@user-session=mate@" ${release}/usr/local/etc/lightdm/lightdm.conf
     cp extra/dm/slick-greeter.conf ${release}/usr/local/etc/lightdm/slick-greeter.conf
-    mkdir ${release}/etc/lightdm
-    cp extra/dm/slick-greeter.conf ${release}/etc/lightdm/slick-greeter.conf 
+    cp extra/dm/msd-background-helper ${release}/usr/local/bin/msd-background-helper
+    chmod +x ${release}/usr/local/bin/msd-background-helper
+    cp extra/dm/msd-background-helper.desktop ${release}/usr/local/etc/xdg/autostart/msd-background-helper.desktop
   fi
   setup_xinit
 }
