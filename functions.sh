43a4d36be5a66e6de8d2d739097828c401aa194f
ericbsd <ericturgeon.bsd@gmail.com>
2019-09-02 21:05:22 -0300

    adding gbi to packages list for mate xfce and kde

--- functions.sh
+++ functions.sh
@@ -177,7 +177,7 @@ extra_config()
   git_pc_sysinstall
   ## git_gbi is for development testing and gbi should be
   ## remove from the package list to avoid conflict
-  git_gbi
+  # git_gbi
   setup_liveuser
   setup_base
   if [ "${desktop}" == "kde" ] ; then
