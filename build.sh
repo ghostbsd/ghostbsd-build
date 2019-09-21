129bd0a2f7e845cd7b905414f8630b3bc54976ae
vimanuelt <vimanuelt@fastmail.fm>
2019-09-21 21:00:31 +0900

    Remove stages

--- build.sh
+++ build.sh
@@ -6,47 +6,16 @@
 determine_desktop
 validate_user
 validate_kernrel
-
-while :
-do
-  case $stage in
-    'stage1')
-      echo "***** Starting Stage 1 *****" 1>&2
-      workspace
-      base
-      packages_software
-      echo "***** Completed Stage 1 *****" 1>&2
-      break  
-      ;;
-    'stage2')
-      echo "***** Starting Stage 2 *****" 1>&2
-      #compress_packages
-      user
-      xorg
-      rc
-      extra_config
-      uzip
-      ramdisk
-      mfs
-      boot
-      image
-      echo "***** Completed Stage 2 *****" 1>&2  
-      break
-      ;;
-    *)
-      workspace
-      base
-      packages_software
-      #compress_packages
-      user
-      xorg
-      rc
-      extra_config
-      uzip
-      ramdisk
-      mfs
-      boot
-      image
-      ;;
-  esac
-done
+workspace
+base
+packages_software
+#compress_packages
+user
+xorg
+rc
+extra_config
+uzip
+ramdisk
+mfs
+boot
+image
