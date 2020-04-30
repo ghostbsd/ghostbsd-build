7b74b38453f2879a424e1b30b0eccebd70e44ace
vimanuelt <vimanuelt@fastmail.fm>
2020-04-30 21:00:28 +0900

    Added vboxservice_flags

--- functions.sh
+++ functions.sh
@@ -128,6 +128,7 @@ rc()
   chroot ${release} rc-update add avahi-dnsconfd default
   chroot ${release} rc-update add ntpd default
   chroot ${release} sysrc -f /etc/rc.conf ntpd_sync_on_start="YES"
+  chroot ${release} sysrc -f /etc/rc.conf vboxservice_flags="--disable-timesync"
 }
 
 user()
