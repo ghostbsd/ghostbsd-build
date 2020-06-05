bba31959071a09e4ab6eee23863fb2d58e391ca4
ericbsd <ericturgeon.bsd@gmail.com>
2020-06-04 22:07:37 -0300

    remove profile from ghostbsd-build moved to src #69

--- extra/common-base-setting.sh
+++ extra/common-base-setting.sh
@@ -5,7 +5,6 @@ set -e -u
 patch_etc_files()
 {
   cat ${cwd}/extra/common-base-setting/patches/boot/loader.conf.extra >> ${release}/boot/loader.conf
-  cat ${cwd}/extra/common-base-setting/patches/etc/profile.extra >> ${release}/etc/profile
   cat ${cwd}/extra/common-base-setting/patches/etc/devfs.rules.extra >> ${release}/etc/devfs.rules
   cat ${cwd}/extra/common-base-setting/patches/etc/fstab.extra >> ${release}/etc/fstab
 }
