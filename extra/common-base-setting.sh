411e559a69366c6ec64eb99d1edaca14a68c3c7a
ericbsd <ericturgeon.bsd@gmail.com>
2020-12-07 21:11:04 -0400

    adding extra fstab support for linux steam  #119

--- extra/common-base-setting.sh
+++ extra/common-base-setting.sh
@@ -7,6 +7,9 @@ patch_etc_files()
   cat ${cwd}/extra/common-base-setting/patches/boot/loader.conf.extra >> ${release}/boot/loader.conf
   cat ${cwd}/extra/common-base-setting/patches/etc/devfs.rules.extra >> ${release}/etc/devfs.rules
   cat ${cwd}/extra/common-base-setting/patches/etc/fstab.extra >> ${release}/etc/fstab
+  mkdir -p ${release}/compat/linux/sys
+  mkdir -p ${release}/compat/linux/dev/shm
+  mkdir -p ${release}/compat/linux/sys
 }
 
 setup_base()
