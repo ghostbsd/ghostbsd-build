46b5001ea1c59e275cb731deb2f2fb59d18219db
Joe Maloney <pkgdemonteam@gmail.com>
2021-02-19 15:04:02 -0500

    Remove vmware support from build scripts
    * Disabling autoadd devices for vmware was breaking input for others.  
    * This should be done in xconfig where it can be detected if a person has vmware first before making these changes after discussing with Eric.

--- extra/finalize.sh
+++ extra/finalize.sh
@@ -13,27 +13,8 @@ set_sudoers()
   sed -i "" -e 's/# %sudo/%sudo/g' ${release}/usr/local/etc/sudoers
 }
 
-vmware_supports()
-{
-printf 'Section "ServerFlags"
-Option "AutoAddDevices" "false"
-EndSection
-Section "InputDevice"
-Identifier "Mouse0"
-Driver "vmmouse"
-Option "Device" "/dev/sysmouse"
-EndSection' > ${release}/usr/local/etc/X11/xorg.conf.d/vmware.conf
-
-printf 'vmware_guest_vmblock_enable="YES"
-vmware_guest_vmhgfs_enable="YES"
-vmware_guest_vmmemctl_enable="YES"
-vmware_guest_vmxnet_enable="YES"
-vmware_guestd_enable="YES"' > ${release}/etc/rc.conf.d/vmware.conf
-}
-
 final_setup()
 {
   default_ghostbsd_rc_conf
   set_sudoers
-  vmware_supports
 }
