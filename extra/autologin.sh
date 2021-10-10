4e575b2e5517e221888ab227fca27801cd067f93
ericbsd <ericturgeon.bsd@gmail.com>
2021-10-10 19:33:31 -0300

    Update autologin to be poxis compliant

--- extra/autologin.sh
+++ extra/autologin.sh
@@ -5,28 +5,30 @@ set -e -u
 
 setup_autologin()
 {
-  echo "# ${liveuser} user autologin" >> ${release}/etc/gettytab
-  echo "${liveuser}:\\" >> ${release}/etc/gettytab
-  echo ":al=${liveuser}:ht:np:sp#115200:" >> ${release}/etc/gettytab
-  sed -i "" "/ttyv0/s/Pc/${liveuser}/g" ${release}/etc/ttys
-  mkdir -p ${release}/usr/home/${liveuser}/.config/fish
-  printf 'set tty (tty)
-  if test $tty = \"/dev/ttyv0\"
+  {
+    echo "# ${liveuser} user autologin"
+    echo "${liveuser}:\\"
+    echo ":al=${liveuser}:ht:np:sp#115200:"
+  } >> "${release}/etc/gettytab"
+  sed -i "" "/ttyv0/s/Pc/${liveuser}/g" "${release}/etc/ttys"
+  mkdir -p "${release}/usr/home/${liveuser}/.config/fish"
+  printf "set tty (tty)
+  if test \$tty = \"/dev/ttyv0\"
     startx
     sleep 1
     sudo xconfig auto
     sleep 1
     startx
   end
-' > ${release}/usr/home/${liveuser}/.config/fish/config.fish
-  chmod 765 ${release}/usr/home/${liveuser}/.config/fish/config.fish
+" > "${release}/usr/home/${liveuser}/.config/fish/config.fish"
+  chmod 765 "${release}/usr/home/${liveuser}/.config/fish/config.fish"
 
   # setup root
-  mkdir -p ${release}/root/.config/fish
-  printf 'set tty (tty)
-  if test $tty = \"/dev/ttyv0\"
+  mkdir -p "${release}/root/.config/fish"
+  printf "set tty (tty)
+  if test \$tty = \"/dev/ttyv0\"
     exec startx
   end
-' > ${release}/root/.config/fish/config.fish
-  chmod 765 ${release}/root/.config/fish/config.fish
+" > "${release}/root/.config/fish/config.fish"
+  chmod 765 "${release}/root/.config/fish/config.fish"
 }
