9cba483958a5cbcc76fceec27c103f668c010bbd
ericbsd <ericturgeon.bsd@gmail.com>
2021-10-10 17:04:22 -0300

    Changed autologin.sh to support the OEM installer

--- extra/autologin.sh
+++ extra/autologin.sh
@@ -10,32 +10,23 @@ setup_autologin()
   echo ":al=${liveuser}:ht:np:sp#115200:" >> ${release}/etc/gettytab
   sed -i "" "/ttyv0/s/Pc/${liveuser}/g" ${release}/etc/ttys
   mkdir -p ${release}/usr/home/${liveuser}/.config/fish
-  if [ -f "${release}/usr/local/bin/xconfig" ] ; then
-    printf '# if not test -f /tmp/.xstarted
-  # touch /tmp/.xstarted
-  set tty (tty)
+  printf 'set tty (tty)
   if test $tty = \"/dev/ttyv0\"
-    # sudo xconfig auto
-    # sleep 1
-    echo "X configuation completed"
+    startx
+    sleep 1
+    sudo xconfig auto
     sleep 1
     startx
   end
-#end
 ' > ${release}/usr/home/${liveuser}/.config/fish/config.fish
   chmod 765 ${release}/usr/home/${liveuser}/.config/fish/config.fish
-  fi
 
+  # setup root
   mkdir -p ${release}/root/.config/fish
-  if [ -f "${release}/usr/local/bin/xconfig" ] ; then
-    printf '# if not test -f /tmp/.xstarted
-  # touch /tmp/.xstarted
-  set tty (tty)
+  printf 'set tty (tty)
   if test $tty = \"/dev/ttyv0\"
     exec startx
   end
-#end
 ' > ${release}/root/.config/fish/config.fish
   chmod 765 ${release}/root/.config/fish/config.fish
-  fi
 }
