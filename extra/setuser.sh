5e3a7297453f1900fb18a314c549bf8af1a36b3b
ericbsd <ericturgeon.bsd@gmail.com>
2021-10-25 08:24:36 -0300

    Changed the set user to support community build

--- extra/setuser.sh
+++ extra/setuser.sh
@@ -4,27 +4,28 @@ set -e -u
 
 setup_liveuser()
 {
-  #${release} su ${liveuser} -c "mkdir -p /usr/home/${liveuser}/Desktop"
+  if [ "${desktop}" = "mate" ] ; then
+    chroot "${release}" su "${liveuser}" -c "mkdir -p /usr/home/${liveuser}/.config/gtk-3.0"
+    chroot "${release}" su "${liveuser}" -c "echo '[Settings]' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
+    chroot "${release}" su "${liveuser}" -c "echo 'gtk-application-prefer-dark-theme = false' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
+    chroot "${release}" su "${liveuser}" -c "echo 'gtk-theme-name = Vimix' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
+    chroot "${release}" su "${liveuser}" -c "echo 'gtk-icon-theme-name = Vivacious-Colors-Dark' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
+    chroot "${release}" su "${liveuser}" -c "echo 'gtk-font-name = Droid Sans Bold 12' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
+    mkdir -p "${release}/root/.config/gtk-3.0"
+    {
+      echo '[Settings]'
+      echo 'gtk-application-prefer-dark-theme = false'
+      echo 'gtk-theme-name = Vimix'
+      echo 'gtk-icon-theme-name = Vivacious-Colors-Dark'
+      echo 'gtk-font-name = Droid Sans Bold 12'
+    } > "${release}/root/.config/gtk-3.0/settings.ini"
+  else
+    ${release} su "${liveuser}" -c "mkdir -p /usr/home/${liveuser}/Desktop"
 
-  #if [ -e ${release}/usr/local/share/applications/gbi.desktop ] ; then
-  #  chroot ${release} su ${liveuser} -c  "cp -af /usr/local/share/applications/gbi.desktop /usr/home/${liveuser}/Desktop"
-  #  chroot ${release} su ${liveuser} -c  "chmod +x /usr/home/${liveuser}/Desktop/gbi.desktop"
-  #  sed -i '' -e 's/NoDisplay=true/NoDisplay=false/g' ${release}/usr/home/${liveuser}/Desktop/gbi.desktop
-  #fi
-
-  chroot "${release}" su "${liveuser}" -c "mkdir -p /usr/home/${liveuser}/.config/gtk-3.0"
-  chroot "${release}" su "${liveuser}" -c "echo '[Settings]' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
-  chroot "${release}" su "${liveuser}" -c "echo 'gtk-application-prefer-dark-theme = false' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
-  chroot "${release}" su "${liveuser}" -c "echo 'gtk-theme-name = Vimix' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
-  chroot "${release}" su "${liveuser}" -c "echo 'gtk-icon-theme-name = Vivacious-Colors-Dark' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
-  chroot "${release}" su "${liveuser}" -c "echo 'gtk-font-name = Droid Sans Bold 12' >> /usr/home/${liveuser}/.config/gtk-3.0/settings.ini"
-
-  mkdir -p "${release}/root/.config/gtk-3.0"
-  {
-    echo '[Settings]'
-    echo 'gtk-application-prefer-dark-theme = false'
-    echo 'gtk-theme-name = Vimix'
-    echo 'gtk-icon-theme-name = Vivacious-Colors-Dark'
-    echo 'gtk-font-name = Droid Sans Bold 12'
-  } > "${release}/root/.config/gtk-3.0/settings.ini"
+    if [ -e "${release}/usr/local/share/applications/gbi.desktop" ] ; then
+     chroot "${release}" su "${liveuser}" -c  "cp -af /usr/local/share/applications/gbi.desktop /usr/home/${liveuser}/Desktop"
+     chroot "${release}" su "${liveuser}" -c  "chmod +x /usr/home/${liveuser}/Desktop/gbi.desktop"
+     sed -i '' -e 's/NoDisplay=true/NoDisplay=false/g' "${release}/usr/home/${liveuser}/Desktop/gbi.desktop"
+    fi
+  fi
 }
