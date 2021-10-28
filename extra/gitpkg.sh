e015a6fb40759c24392709dfba689492968f4eb7
ericbsd <ericturgeon.bsd@gmail.com>
2021-10-28 20:03:16 -0300

    change the repos of gbi

--- extra/gitpkg.sh
+++ extra/gitpkg.sh
@@ -28,7 +28,7 @@ git_gbi()
 {
   if [ ! -d "${release}/gbi" ]; then
     echo "Downloading gbi from GitHub"
-    git clone -b setup-station https://github.com/GhostBSD/gbi.git "${release}/gbi" >/dev/null 2>&1
+    git clone -b network-setup https://github.com/GhostBSD/gbi.git "${release}/gbi" >/dev/null 2>&1
     # cp -R /usr/home/ericbsd/projects/ghostbsd/gbi ${release}/gbi
   fi
 
