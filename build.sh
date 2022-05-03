6a580e79d7ef34a57989b7ace8fac7881cd09a9b
Vester Imanuel Thacker (Vic) <92239292+vimanuelt@users.noreply.github.com>
2022-05-03 22:28:49 +0900

    Remove scripts from cwd
    As the build.sh script was moved from a scripts directory to the ghostbsd-build root directory, the sed function to remove scripts from path is no longer necessary. Also, the scripts directory no longer exists and was deprecated a very long time ago.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e -u
 
-export cwd="`realpath | sed 's|/scripts||g'`"
+export cwd="`realpath`"
 # Only run as superuser
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
