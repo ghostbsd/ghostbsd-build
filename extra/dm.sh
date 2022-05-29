1c649afbc567cb553fc77a33856436385c6ebf94
Eric Turgeon <4249848+ericbsd@users.noreply.github.com>
2022-05-29 07:57:33 -0400

    Merge pull request #159 from vimanuelt/patch-7
    Remove kde and cinnamon entries

--- extra/dm.sh
+++ extra/dm.sh
@@ -4,15 +4,11 @@ set -e -u
 
 lightdm_setup()
 {
-  sed -i '' "s@#greeter-session=example-gtk-gnome@greeter-session=slick-greeter@" "${release}/usr/local/etc/lightdm/lightdm.conf"
-  if [ "${desktop}" = "xfce" ] ; then
-    sed -i '' "s@#user-session=default@user-session=xfce@" "${release}/usr/local/etc/lightdm/lightdm.conf"
-  elif [ "${desktop}" = "mate" ] ; then
-    sed -i '' "s@#user-session=default@user-session=mate@" "${release}/usr/local/etc/lightdm/lightdm.conf"
-  elif [ "${desktop}" = "kde" ] ; then
-    sed -i '' "s@#user-session=default@user-session=plasma@" "${release}/usr/local/etc/lightdm/lightdm.conf"
-  elif [ "${desktop}" = "cinnamon" ] ; then
-    sed -i '' "s@#user-session=default@user-session=cinnamon@" "${release}/usr/local/etc/lightdm/lightdm.conf"
+  sed -i '' "s@#greeter-session=example-gtk-gnome@greeter-session=slick-greeter@" ${release}/usr/local/etc/lightdm/lightdm.conf
+  if [ "${desktop}" == "xfce" ] ; then
+    sed -i '' "s@#user-session=default@user-session=xfce@" ${release}/usr/local/etc/lightdm/lightdm.conf
+  elif [ "${desktop}" == "mate" ] ; then
+    sed -i '' "s@#user-session=default@user-session=mate@" ${release}/usr/local/etc/lightdm/lightdm.conf
   fi
   setup_xinit
 
@@ -20,25 +16,15 @@ lightdm_setup()
 
 setup_xinit()
 {
-  if [ "${desktop}" = "mate" ] ; then
-    # echo "gsettings set org.mate.SettingsDaemon.plugins.housekeeping active true &" > ${release}/usr/home/${liveuser}/.xinitrc
-    # echo "gsettings set org.mate.screensaver lock-enabled false &" >> ${release}/usr/home/${liveuser}/.xinitrc
-    # # echo "gsettings set org.mate.lockdown disable-lock-screen true &" >> ${release}/usr/home/${liveuser}/.xinitrc
-    # # echo "gsettings set org.mate.lockdown disable-user-switching true &" >> ${release}/usr/home/${liveuser}/.xinitrc
-    # echo "exec ck-launch-session mate-session" >> ${release}/usr/home/${liveuser}/.xinitrc
-    echo "exec marco &" > "${release}/usr/home/${liveuser}/.xinitrc"
-    echo "exec feh --bg-fill /usr/local/share/backgrounds/ghostbsd/Arizona_Desert_Monument.jpg &" >> "${release}/usr/home/${liveuser}/.xinitrc"
-    echo "exec sudo install-station" >> "${release}/usr/home/${liveuser}/.xinitrc"
-    chmod 765 "${release}/usr/home/${liveuser}/.xinitrc"
-    echo "exec ck-launch-session mate-session" > "${release}/root/.xinitrc"
-  elif [ "${desktop}" = "xfce" ] ; then
-    echo "exec ck-launch-session startxfce4" > "${release}/usr/home/${liveuser}/.xinitrc"
-    echo "exec ck-launch-session startxfce4" > "${release}/root/.xinitrc"
-  elif [ "${desktop}" = "cinnamon" ] ; then
-    echo "exec ck-launch-session cinnamon-session" > "${release}/usr/home/${liveuser}/.xinitrc"
-    echo "exec ck-launch-session cinnamon-session" > "${release}/root/.xinitrc"
-  elif [ "${desktop}" = "kde" ] ; then
-    echo "exec ck-launch-session startplasma-x11"> "${release}/usr/home/${liveuser}/.xinitrc"
-    echo "exec ck-launch-session startplasma-x11" > "${release}/root/.xinitrc"
+  if [ "${desktop}" == "mate" ] ; then
+    echo "gsettings set org.mate.SettingsDaemon.plugins.housekeeping active true &" > ${release}/usr/home/${liveuser}/.xinitrc
+    echo "gsettings set org.mate.screensaver lock-enabled false &" >> ${release}/usr/home/${liveuser}/.xinitrc
+    echo "gsettings set org.mate.lockdown disable-lock-screen true &" >> ${release}/usr/home/${liveuser}/.xinitrc
+    echo "gsettings set org.mate.lockdown disable-user-switching true &" >> ${release}/usr/home/${liveuser}/.xinitrc
+    echo "exec ck-launch-session mate-session" >> ${release}/usr/home/${liveuser}/.xinitrc
+    echo "exec ck-launch-session mate-session" > ${release}/root/.xinitrc
+  elif [ "${desktop}" == "xfce" ] ; then
+    echo "exec ck-launch-session startxfce4" > ${release}/usr/home/${liveuser}/.xinitrc
+    echo "exec ck-launch-session startxfce4" > ${release}/root/.xinitrc
   fi
 }
