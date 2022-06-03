14e48d567a9dadb5ac04f2cbcacf2b37dc6e21e5
ericbsd <ericturgeon.bsd@gmail.com>
2022-06-03 17:51:38 -0300

    Fixed tmpfs for new shm setting from base

--- extra/common-base-setting.sh
+++ extra/common-base-setting.sh
@@ -4,11 +4,10 @@ set -e -u
 
 patch_etc_files()
 {
-  cat ${cwd}/extra/common-base-setting/patches/etc/devfs.rules.extra >> ${release}/etc/devfs.rules
-  cat ${cwd}/extra/common-base-setting/patches/etc/fstab.extra >> ${release}/etc/fstab
-  mkdir -p ${release}/compat/linux/sys
-  mkdir -p ${release}/compat/linux/dev/shm
-  mkdir -p ${release}/compat/linux/sys
+  cat "${cwd}/extra/common-base-setting/patches/etc/devfs.rules.extra" >> "${release}/etc/devfs.rules"
+  cat "${cwd}/extra/common-base-setting/patches/etc/fstab.extra" >> "${release}/etc/fstab"
+  mkdir -p "${release}/compat/linux/dev/shm"
+  mkdir -p "${release}/compat/linux/sys"
 }
 
 setup_base()
