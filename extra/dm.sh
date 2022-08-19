7b622ebedf2d57adabe905245ae56436345a30fb
ericbsd <ericturgeon.bsd@gmail.com>
2022-08-19 15:15:12 -0300

    Setup install station in .xinitrc

--- extra/dm.sh
+++ extra/dm.sh
@@ -17,13 +17,16 @@ lightdm_setup()
 setup_xinit()
 {
   if [ "${desktop}" = "mate" ] ; then
-    echo "gsettings set org.mate.SettingsDaemon.plugins.housekeeping active true &" > "${release}/usr/home/${liveuser}/.xinitrc"
-    {
-      echo "gsettings set org.mate.screensaver lock-enabled false &"
-      echo "gsettings set org.mate.lockdown disable-lock-screen true &"
-      echo "gsettings set org.mate.lockdown disable-user-switching true &"
-    } >> "${release}/usr/home/${liveuser}/.xinitrc"
-    echo "exec ck-launch-session mate-session" >> "${release}/usr/home/${liveuser}/.xinitrc"
+    # echo "gsettings set org.mate.SettingsDaemon.plugins.housekeeping active true &" > "${release}/usr/home/${liveuser}/.xinitrc"
+    # {
+    #   echo "gsettings set org.mate.screensaver lock-enabled false &"
+    #   echo "gsettings set org.mate.lockdown disable-lock-screen true &"
+    #   echo "gsettings set org.mate.lockdown disable-user-switching true &"
+    # } >> "${release}/usr/home/${liveuser}/.xinitrc"
+    echo "exec marco &" > "${release}/usr/home/${liveuser}/.xinitrc"
+    echo "exec feh --bg-fill /usr/local/share/backgrounds/ghostbsd/Arizona_Desert_Monument.jpg &" >> "${release}/usr/home/${liveuser}/.xinitrc"
+    echo "exec sudo install-station" >> "${release}/usr/home/${liveuser}/.xinitrc"
+    chmod 765 "${release}/usr/home/${liveuser}/.xinitrc"
     echo "exec ck-launch-session mate-session" > "${release}/root/.xinitrc"
   elif [ "${desktop}" = "xfce" ] ; then
     echo "exec ck-launch-session startxfce4" > "${release}/usr/home/${liveuser}/.xinitrc"
