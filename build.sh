838ebef5776a6b72f13ee6aecf70bd1d4b0b8857
ericbsd <ericturgeon.bsd@gmail.com>
2022-10-25 20:56:38 -0300

    Added yes to the pkg update of fetch_x_drivers_packages

--- build.sh
+++ build.sh
@@ -166,9 +166,9 @@ packages_software()
 fetch_x_drivers_packages()
 {
   mkdir ${release}/xdrivers
-  pkg -R "${cwd}/pkg/" update
-  pkg -R "${cwd}/pkg/" rquery -x -r ${PKGCONG} '%n %n-%v.pkg' 'nvidia-driver' > ${release}/xdrivers/drivers-list
-  pkg_list="$(pkg -R "${cwd}/pkg/" rquery -x -r ${PKGCONG} '%n-%v.pkg' 'nvidia-driver')"
+  yes | pkg -R ${cwd}/pkg/ update
+  echo "$(pkg -R ${cwd}/pkg/ rquery -x -r ${PKGCONG} '%n %n-%v.pkg' 'nvidia-driver')" > ${release}/xdrivers/drivers-list
+  pkg_list="$(pkg -R ${cwd}/pkg/ rquery -x -r ${PKGCONG} '%n-%v.pkg' 'nvidia-driver')"
   for line in $pkg_list ; do
     fetch -o ${release}/xdrivers "${pkg_url}/All/$line"
   done
