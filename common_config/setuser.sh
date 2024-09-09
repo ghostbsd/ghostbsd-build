34b0a1a55978b63bb3f064bd2215fd5ae316760c
V.I. Thacker <vic.thacker@fastmail.fm>
2024-09-09 14:37:18 +0900

    Update live_user in setuser.sh

--- common_config/setuser.sh
+++ common_config/setuser.sh
@@ -5,20 +5,20 @@ set -e -u
 set_user()
 {
   chroot "${release}" pw usermod -s /usr/local/bin/fish -n root
-  chroot "${release}" pw useradd "${liveuser}" \
-  -c "GhostBSD Live User" -d "/home/${liveuser}" \
+  chroot "${release}" pw useradd "${live_user}" \
+  -c "GhostBSD Live User" -d "/home/${live_user}" \
   -g wheel -G operator -m -s /usr/local/bin/fish -k /usr/share/skel -w none
 }
 
 ghostbsd_setup_liveuser()
 {
   set_user
-  chroot "${release}" su "${liveuser}" -c "mkdir -p /home/${liveuser}/.config/gtk-3.0"
-  chroot "${release}" su "${liveuser}" -c "echo '[Settings]' >> /home/${liveuser}/.config/gtk-3.0/settings.ini"
-  chroot "${release}" su "${liveuser}" -c "echo 'gtk-application-prefer-dark-theme = false' >> /home/${liveuser}/.config/gtk-3.0/settings.ini"
-  chroot "${release}" su "${liveuser}" -c "echo 'gtk-theme-name = Vimix' >> /home/${liveuser}/.config/gtk-3.0/settings.ini"
-  chroot "${release}" su "${liveuser}" -c "echo 'gtk-icon-theme-name = Vivacious-Colors-Dark' >> /home/${liveuser}/.config/gtk-3.0/settings.ini"
-  chroot "${release}" su "${liveuser}" -c "echo 'gtk-font-name = Droid Sans Bold 12' >> /home/${liveuser}/.config/gtk-3.0/settings.ini"
+  chroot "${release}" su "${live_user}" -c "mkdir -p /home/${live_user}/.config/gtk-3.0"
+  chroot "${release}" su "${live_user}" -c "echo '[Settings]' >> /home/${live_user}/.config/gtk-3.0/settings.ini"
+  chroot "${release}" su "${live_user}" -c "echo 'gtk-application-prefer-dark-theme = false' >> /home/${live_user}/.config/gtk-3.0/settings.ini"
+  chroot "${release}" su "${live_user}" -c "echo 'gtk-theme-name = Vimix' >> /home/${live_user}/.config/gtk-3.0/settings.ini"
+  chroot "${release}" su "${live_user}" -c "echo 'gtk-icon-theme-name = Vivacious-Colors-Dark' >> /home/${live_user}/.config/gtk-3.0/settings.ini"
+  chroot "${release}" su "${live_user}" -c "echo 'gtk-font-name = Droid Sans Bold 12' >> /home/${live_user}/.config/gtk-3.0/settings.ini"
   mkdir -p "${release}/root/.config/gtk-3.0"
   {
     echo '[Settings]'
@@ -32,11 +32,11 @@ ghostbsd_setup_liveuser()
 community_setup_liveuser()
 {
   set_user
-  chroot "${release}" su "${liveuser}" -c "mkdir -p /home/${liveuser}/Desktop"
+  chroot "${release}" su "${live_user}" -c "mkdir -p /home/${live_user}/Desktop"
 
   if [ -e "${release}/usr/local/share/applications/gbi.desktop" ] ; then
-   chroot "${release}" su "${liveuser}" -c  "cp -af /usr/local/share/applications/gbi.desktop /home/${liveuser}/Desktop"
-   chroot "${release}" su "${liveuser}" -c  "chmod +x /home/${liveuser}/Desktop/gbi.desktop"
-   sed -i '' -e 's/NoDisplay=true/NoDisplay=false/g' "${release}/home/${liveuser}/Desktop/gbi.desktop"
+   chroot "${release}" su "${live_user}" -c  "cp -af /usr/local/share/applications/gbi.desktop /home/${live_user}/Desktop"
+   chroot "${release}" su "${live_user}" -c  "chmod +x /home/${live_user}/Desktop/gbi.desktop"
+   sed -i '' -e 's/NoDisplay=true/NoDisplay=false/g' "${release}/home/${live_user}/Desktop/gbi.desktop"
   fi
 }
