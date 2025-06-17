b8c0acb3f8c8787c7c863e0ddb81799867567b97
pkgdemon <pkgdemonteam@gmail.com>
2025-06-17 15:11:06 -0500

    Changes to test autologin for Gershwin

--- desktop_config/gershwin.sh
+++ desktop_config/gershwin.sh
@@ -1,1 +1,29 @@
 #!/bin/sh
+
+set -e -u
+
+. "${cwd}/common_config/autologin.sh"
+. "${cwd}/common_config/base-setting.sh"
+. "${cwd}/common_config/finalize.sh"
+. "${cwd}/common_config/setuser.sh"
+
+lightdm_setup()
+{
+  sed -i '' "s@#greeter-session=example-gtk-gnome@greeter-session=slick-greeter@" "${release}/usr/local/etc/lightdm/lightdm.conf"
+  sed -i '' "s@#user-session=default@user-session=gershwin@" "${release}/usr/local/etc/lightdm/lightdm.conf"
+}
+
+setup_xinit()
+{
+  chroot "${release}" su "${live_user}" -c "echo 'exec /usr/local/bin/gershwin-x11' > /home/${live_user}/.xinitrc"
+  chroot "${release}" su "${live_user}" -c "echo 'exec /usr/local/bin/gershwin-x11' > /home/${live_user}/.xinitrc"
+  echo "exec /usr/local/bin/gershwin-x11" > "${release}/root/.xinitrc"
+  echo "exec /usr/local/bin/gershwin-x11" > "${release}/usr/share/skel/dot.xinitrc"
+}
+
+patch_etc_files
+community_setup_liveuser_gershwin
+community_setup_autologin
+lightdm_setup
+setup_xinit
+final_setup
